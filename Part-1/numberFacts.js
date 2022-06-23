7c4ca40336ee4a14123d3f783d2d4441548801e1
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-06-22 22:16:33 -0400

    added async/await approaches

--- Part-1/numberFacts.js
+++ Part-1/numberFacts.js
@@ -1,39 +1,72 @@
-// Part 1: Number Facts
+// Part 1: Number Facts ****************************************************************
 
 let baseURL = "http://numbersapi.com";
 
-// #1
-axios.get(`${baseURL}/3?json`)
-  .then(response => {
-    console.log(`LuckyNums #1 ${response.data}`);
-  })
-  .catch(err => {
-    console.log(`Oops, there was a problem :( ${err}`);
-  });
+// #1 with Promises
+// axios.get(`${baseURL}/3?json`)
+//   .then(response => {
+//     console.log(`LuckyNums #1 ${response.data}`);
+//   })
+//   .catch(err => {
+//     console.log(`Oops, there was a problem :( ${err}`);
+//   });
 
-// #2
-let facts = axios.get(`${baseURL}/1..3`)
-  .then(response => {
-    facts = response.data;
-    for (const fact in facts) {
-        $("#results-list").append(`<li>${facts[fact]}</li>`)}
-    })
-  .catch(err => {
-    console.log(`Oops, there was a problem :( ${err}`);
-  });
-
-// #3
-
-let fourNumberFacts = [];
-
-for (let i = 1; i < 5; i++) {
-  fourNumberFacts.push(
-    axios.get(`${baseURL}/3`)
-  );
+// #1 with Async/Await
+async function getLuckyNum() {
+  let response = await axios.get(`${baseURL}/3?json`)
+  console.log(`LuckyNums #1 ${response.data.text}`)
 }
 
-Promise.all(fourNumberFacts)
-  .then(numArr => (
-    numArr.forEach(fact => $("#results-list-2").append(`<li>${fact.data}</li>`))
-  ))
-  .catch(err => console.log(err));
+getLuckyNum()
+
+// #2 with Promises
+// let facts = axios.get(`${baseURL}/1..3`)
+//   .then(response => {
+//     facts = response.data;
+//     for (const fact in facts) {
+//         $("#results-list").append(`<li>${facts[fact]}</li>`)}
+//     })
+//   .catch(err => {
+//     console.log(`Oops, there was a problem :( ${err}`);
+//   });
+
+// #2 with Async/Await
+async function getFacts() {
+  let {data: facts} = await axios.get(`${baseURL}/1..3`)
+  for (const fact in facts) {
+    $("#results-list").append(`<li>${facts[fact]}</li>`)
+  }
+}
+
+getFacts()
+
+// #3 with Promises
+
+// let fourNumberFacts = [];
+
+// for (let i = 1; i < 5; i++) {
+//   fourNumberFacts.push(
+//     axios.get(`${baseURL}/3`)
+//   );
+// }
+
+// Promise.all(fourNumberFacts)
+//   .then(numArr => (
+//     numArr.forEach(fact => $("#results-list-2").append(`<li>${fact.data}</li>`))
+//   ))
+//   .catch(err => console.log(err));
+
+
+// #3 with Async/Await
+
+async function getNumberFacts() {
+  let fourNumberFacts = await Promise.all([
+    axios.get(`${baseURL}/3`),
+    axios.get(`${baseURL}/3`),
+    axios.get(`${baseURL}/3`)
+  ])
+
+  fourNumberFacts.forEach(fact => $("#results-list-2").append(`<li>${fact.data}</li>`))
+}
+
+getNumberFacts()
--- Part-2/deckOfCards.js
+++ Part-2/deckOfCards.js
@@ -1,57 +1,103 @@
 let baseURL = "http://deckofcardsapi.com/api/deck"
 
 
-// #1
-axios.get(`${baseURL}/new/draw/?count=1`)
-  .then(response => {
-    console.log(`#1  Your card is: A ${response.data.cards[0].value.toLowerCase()} of ${response.data.cards[0].suit.toLowerCase()}`);
-  })
-  .catch(err => {
-    console.log(`Oops, there was a problem :( ${err}`);
+// #1 with Promises
+// axios.get(`${baseURL}/new/draw/?count=1`)
+//   .then(response => {
+//     console.log(`#1  Your card is: A ${response.data.cards[0].value.toLowerCase()} of ${response.data.cards[0].suit.toLowerCase()}`);
+//   })
+//   .catch(err => {
+//     console.log(`Oops, there was a problem :( ${err}`);
+//   });
+
+// #1 with Async/Await
+async function drawCard() {
+  let response = await axios.get(`${baseURL}/new/draw/?count=1`)
+  console.log(`#1  Your card is: A ${response.data.cards[0].value.toLowerCase()} of ${response.data.cards[0].suit.toLowerCase()}`)
+}
+
+drawCard()
+
+// #2 with Promises
+
+// axios.get(`${baseURL}/new/draw/?count=2`)
+// .then(response => {
+//   firstCard = response.data.cards[0]
+//   secondCard = response.data.cards[1]
+//   console.log(`#2  Your first card is: A ${response.data.cards[0].value.toLowerCase()} of ${response.data.cards[0].suit.toLowerCase()} and your second card is : A ${response.data.cards[1].value.toLowerCase()} of ${response.data.cards[1].suit.toLowerCase()}`)  
+// })
+
+// #2 with Async/Await
+
+async function drawTwo() {
+  let response = await axios.get(`${baseURL}/new/draw/?count=2`)
+  console.log(`#2  Your first card is: A ${response.data.cards[0].value.toLowerCase()} of ${response.data.cards[0].suit.toLowerCase()} and your second card is : A ${response.data.cards[1].value.toLowerCase()} of ${response.data.cards[1].suit.toLowerCase()}`) 
+}
+
+drawTwo()
+
+
+// #3 with Promises
+
+// let deckId = null;
+// let $btn = $('button');
+// let $cardArea = $('#card-area');
+
+// axios.get(`${baseURL}/new/shuffle/`)
+//   .then(response => {
+//     deckId = response.data.deck_id;
+//     console.log(deckId)
+//     $btn.show();
+//   })
+//   .catch(err => {
+//     console.log(`Oops, there was a problem :( ${err}`);
+//   });
+
+
+// $btn.on('click', function() {
+//     axios.get(`${baseURL}/${deckId}/draw/`).then(response => {
+//       console.log(response.data)
+//       let cardSrc = response.data.cards[0].image;
+//       let angle = Math.random() * 90 - 45;
+//       let randomX = Math.random() * 40 - 20;
+//       let randomY = Math.random() * 40 - 20;
+//       $cardArea.append(
+//         $('<img>', {
+//           src: cardSrc,
+//           css: {
+//             transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+//           }
+//         })
+//       );
+//       if (response.data.remaining === 0) $btn.remove();
+//     });
+//   });
+
+// #3 with Async/Await
+
+async function setup() {
+  let $btn = $('button');
+  let $cardArea = $('#card-area');
+
+  let deckData = await $.getJSON(`${baseURL}/new/shuffle/`);
+  $btn.show().on('click', async function() {
+    let cardData = await $.getJSON(`${baseURL}/${deckData.deck_id}/draw/`);
+    let cardSrc = cardData.cards[0].image;
+    let angle = Math.random() * 90 - 45;
+    let randomX = Math.random() * 40 - 20;
+    let randomY = Math.random() * 40 - 20;
+    $cardArea.append(
+      $('<img>', {
+        src: cardSrc,
+        css: {
+          transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+        }
+      })
+    );
+    if (cardData.remaining === 0) $btn.remove();
   });
-
-// #2
-
-axios.get(`${baseURL}/new/draw/?count=2`)
-.then(response => {
-  firstCard = response.data.cards[0]
-  secondCard = response.data.cards[1]
-  console.log(`#2  Your first card is: A ${response.data.cards[0].value.toLowerCase()} of ${response.data.cards[0].suit.toLowerCase()} and your second card is : A ${response.data.cards[1].value.toLowerCase()} of ${response.data.cards[1].suit.toLowerCase()}`)  
-})
+}
+setup();
 
 
-// #3
-let deckId = null;
-let $btn = $('button');
-let $cardArea = $('#card-area');
-
-axios.get(`${baseURL}/new/shuffle/`)
-  .then(response => {
-    deckId = response.data.deck_id;
-    console.log(deckId)
-    $btn.show();
-  })
-  .catch(err => {
-    console.log(`Oops, there was a problem :( ${err}`);
-  });
-
-
-$btn.on('click', function() {
-    axios.get(`${baseURL}/${deckId}/draw/`).then(response => {
-      console.log(response.data)
-      let cardSrc = response.data.cards[0].image;
-      let angle = Math.random() * 90 - 45;
-      let randomX = Math.random() * 40 - 20;
-      let randomY = Math.random() * 40 - 20;
-      $cardArea.append(
-        $('<img>', {
-          src: cardSrc,
-          css: {
-            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
-          }
-        })
-      );
-      if (response.data.remaining === 0) $btn.remove();
-    });
-  });
 
